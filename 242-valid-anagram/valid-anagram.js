07ccc006695cf61147d1875d1482bf85d1e558bd
vctorye <118642061+vctorye@users.noreply.github.com>
2025-06-23 17:11:24 -0400

    Time: 13 ms (66.77%) | Memory: 59.6 MB (28.33%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+     if (s.length !== t.length) return false
+
+     const count = {}
+
+     for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+     }
+    
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--
+    }
+
+    return true
+};
