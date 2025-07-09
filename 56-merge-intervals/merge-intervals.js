e6e5d723caf99bb85d7a2200464a92a28e9aa114
vctorye <118642061+vctorye@users.noreply.github.com>
2025-07-09 14:09:26 -0400

    Time: 6 ms (94.36%) | Memory: 64.1 MB (78.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -3,22 +3,21 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (!intervals.length) return [];
 
-    intervals.sort((a,b) => a[0] - b[0]);
-
-    const result = [intervals[0]]
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [];
+    let prev = intervals[0];
 
     for (let i = 1; i < intervals.length; i++) {
-        const [start, end] = intervals[i];
-        const last = result[result.length - 1];
-
-        if (start <= last[1]) {
-            last[1] = Math.max(last[1], end);
+        let interval = intervals[i];
+        if (interval[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], interval[1]);
         } else {
-            result.push([start, end]);
+            merged.push(prev);
+            prev = interval;
         }
     }
 
-    return result
+    merged.push(prev);
+    return merged;    
 };
