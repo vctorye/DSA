4022c6051c30ceab91b9b415044501c3b8ca6bc4
vctorye <118642061+vctorye@users.noreply.github.com>
2025-09-30 21:03:43 -0400

    Time: 36 ms (62.73%) | Memory: 73.6 MB (13.59%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,24 +3,23 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-     nums.sort((a, b) => a - b);
-    let res = []
-    for (let i = 0; i < nums.length - 2; i++) {
+    nums.sort((a, b) => a - b);
+    let results = []
+    for (let i = 0; i < nums.length - 2; i ++) {
         if (i > 0 && nums[i] === nums[i-1]) {
             continue;
         }
-
         let left = i + 1, right = nums.length - 1;
-       
-        while (left < right) {
-            let total = nums[i] + nums[left] + nums[right];
 
-            if (total > 0) {
+        while (left < right) {
+            let sum = nums[left] + nums[right] + nums[i];
+
+            if (sum > 0) {
                 right--;
-            } else if (total < 0) {
+            } else if (sum < 0) {
                 left++
             } else {
-                res.push([nums[i], nums[left], nums[right]]);
+                results.push([ nums[i], nums[left], nums[right]])
                 left++
                 while (nums[left] === nums[left - 1] && left < right){
                     left++
@@ -28,6 +27,6 @@ var threeSum = function(nums) {
             }
         }
     }
-    return res
- };
+    return results; 
+};
 
