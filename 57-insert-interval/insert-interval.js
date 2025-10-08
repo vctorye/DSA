3a1ebb595255146ef66447a3e9ba4a97ce5aff4b
vctorye <118642061+vctorye@users.noreply.github.com>
2025-10-08 19:28:27 -0400

    Time: 5 ms (29.68%) | Memory: 61.2 MB (6.28%) - LeetSync

--- 57-insert-interval/insert-interval.js
+++ 57-insert-interval/insert-interval.js
@@ -5,25 +5,21 @@
  */
 // O(n), O(n)
 var insert = function (intervals, newInterval) {
-  let [start, end] = newInterval;
-  let left = [];
-  let right = [];
-  
-  for (const interval of intervals) {
-    const [first, last] = interval;
-	
-	// current interval is smaller than newInterval
-    if (last < start) left.push(interval);
-	
-	// current interval is larger than newInterval
-    else if (first > end) right.push(interval);
-	
-	// there is a overlap
-    else {
-      start = Math.min(start, first);
-      end = Math.max(end, last);
+    let [start, end] = newInterval;
+    let left = [];
+    let right = [];
+
+    for (let interval of intervals) {
+        let [first, last] = interval;
+        if (last < start) {
+            left.push(interval);
+        } else if (first > end) {
+            right.push(interval);
+        } else {
+            start = Math.min(start, first);
+            end = Math.max(end, last);
+        }
     }
-  }
-  
-  return [...left, [start, end], ...right]; 
+
+    return [...left, [start,end], ...right]
 };
