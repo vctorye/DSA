3a3ea1de113e6631102a25be3f9e0e85f72c58cb
vctorye <118642061+vctorye@users.noreply.github.com>
2025-10-09 12:53:18 -0400

    Time: 0 ms (100.00%) | Memory: 53.7 MB (93.26%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -9,30 +9,24 @@ var merge = function(nums1, m, nums2, n) {
      let i = m - 1, j = n - 1, k = m + n - 1;
 
      while (i >= 0 && j >= 0) {
-        // if num1[i] is less than num2[j] then num[k] is num2[j]  
         if (nums1[i] < nums2[j]) {
-            nums1[k] = nums2[j]
-            //decrement for k and j
-            k--;
+            nums1[k] = nums2[j];
             j--
-        } else { // vice versa if num1[i] is greater than num2[j]
+        } else {
             nums1[k] = nums1[i];
-            // decrement for k and i
-            k--
             i--
         }
+        k--
     }
 
-       // if there are any left over then reaplce into array
     while (j >= 0) {
         nums1[k] = nums2[j];
-        k--;
-        j--;
+        j--
+        k--
     }
     while (i >= 0) {
         nums1[k] = nums1[i];
-        k--;
-        i--;
+        i--
+        k--
     }
-
 };
