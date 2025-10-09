175c64f4a75606fd0647579d29167f1e17882e3c
vctorye <118642061+vctorye@users.noreply.github.com>
2025-10-08 21:42:09 -0400

    Time: 3 ms (79.29%) | Memory: 55.9 MB (94.01%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -4,32 +4,30 @@
  */
 var maxAreaOfIsland = function(grid) {
    // set variables 
-    const rows = grid.length;
-    const cols = grid[0].length;
+    let rows = grid.length;
+    let cols = grid[0].length;
     let maxArea = 0;
 
    // set dfs function
     const dfs = (r, c) => {
-        // set bounds 
-        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0
+        // set the bounds
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0;
 
-        // mark visited
-        grid[r][c] = 0;
+        grid[r][c] = 0
         let area = 1;
 
-        // recursively explore all 4 directions and accumulate the area
-        area += dfs(r + 1, c); // Down
-        area += dfs(r - 1, c); // Up
-        area += dfs(r, c + 1); // Right
-        area += dfs(r, c - 1); // Left
-
+        // recursively explore all 4 directions 
+        area += dfs(r + 1, c)
+        area += dfs(r - 1, c)
+        area += dfs(r, c + 1)
+        area += dfs(r, c - 1)
         return area
     }
    // traverse thorugh grid
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+        for (let c = 0; c <cols; c++) {
             if (grid[r][c] === 1) {
-                let area = dfs(r,c);
+                let area = dfs(r,c)
                 maxArea = Math.max(maxArea, area)
             }
         }
