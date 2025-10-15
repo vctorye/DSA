8233c6015b07ea3d51a0ba38fbe131f70857b965
vctorye <118642061+vctorye@users.noreply.github.com>
2025-10-15 00:13:13 -0400

    Time: 10 ms (22.77%) | Memory: 63 MB (34.06%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,21 +3,28 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
+    // check if its a palindrome
     let low = 0, high = s.length - 1;
-    while(low < high) {
+    while (low < high) {
         if (s[low] !== s[high]) {
-            return isPalindrome(low + 1, high, s) || isPalindrome(low, high - 1, s)
+            return isPalindrome(low + 1,high, s) || isPalindrome(low,high - 1, s)
+        } else {
+            low++
+            high--
         }
-        low++, high--
     }
     return true
-    
-}
-function isPalindrome(low, high, str) {
-    while(high > low) {
-        if (str[low] !== str[high]) return false;
 
-        low++, high--
+    // create a helper function 
+    function isPalindrome(low, high, s) {
+        while (low < high) {
+            if (s[low] !== s[high]) {
+                return false
+            } else {
+                low++
+                high--
+            }
+        }
+        return true
     }
-    return true
 } 
