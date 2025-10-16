73ea8ffc9f52416a5f7dbfb9e1b1f5bf512eb9fd
vctorye <118642061+vctorye@users.noreply.github.com>
2025-10-16 12:57:09 -0400

    Time: 0 ms (100.00%) | Memory: 58 MB (52.92%) - LeetSync

--- 112-path-sum/path-sum.js
+++ 112-path-sum/path-sum.js
@@ -12,20 +12,25 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-    // If the node is null, there is no path, so return false
-    if (!root) return false;
+   // If the node is null, there is no path, so return false
+   if (!root) return false;
 
-    // If this is a leaf node (no children), check if the current node's value equals the target sum\
-    if (!root.left && !root.right & targetSum - root.val === 0) return true;
-    
-    // Recursively check the left and right subtrees
-    // Subtract the current node's value from targetSum and pass it down the tree
-    return (
-        hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
-        )
 
-    // Explanation of `||` (OR):
-    // We use `||` because we only need one path to sum to the target.
-    // If either left or right subtree has a path that meets the target sum,
-    // the function will return true, stopping further recursion down other paths.
+   // If this is a leaf node (no children), check if the current node's value equals the target sum\
+   if (!root.left && !root.right & targetSum - root.val === 0) return true;
+  
+   // Recursively check the left and right subtrees
+   // Subtract the current node's value from targetSum and pass it down the tree
+   return (
+       hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+       )
+
+
+   // Explanation of `||` (OR):
+   // We use `||` because we only need one path to sum to the target.
+   // If either left or right subtree has a path that meets the target sum,
+   // the function will return true, stopping further recursion down other paths.
+
+
+
 };
