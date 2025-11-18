a9afa6af2a786df5f2fd5a0d7710b6297f85d763
vctorye <118642061+vctorye@users.noreply.github.com>
2025-11-18 15:29:44 -0500

    Time: 1 ms (46.38%) | Memory: 58.6 MB (16.40%) - LeetSync

--- 112-path-sum/path-sum.js
+++ 112-path-sum/path-sum.js
@@ -12,25 +12,11 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-   // If the node is null, there is no path, so return false
-   if (!root) return false;
+    if (!root) return false
 
+    if (!root.left && !root.right && targetSum === root.val) return true 
 
-   // If this is a leaf node (no children), check if the current node's value equals the target sum\
-   if (!root.left && !root.right & targetSum - root.val === 0) return true;
-  
-   // Recursively check the left and right subtrees
-   // Subtract the current node's value from targetSum and pass it down the tree
-   return (
-       hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
-       )
-
-
-   // Explanation of `||` (OR):
-   // We use `||` because we only need one path to sum to the target.
-   // If either left or right subtree has a path that meets the target sum,
-   // the function will return true, stopping further recursion down other paths.
-
+    return (hasPathSum(root.right, targetSum - root.val) || hasPathSum(root.left, targetSum - root.val))
 
 
 };
