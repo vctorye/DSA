fe9f28e9f8ee05cba001146ac97b6b38c3de544a
vctorye <118642061+vctorye@users.noreply.github.com>
2025-11-17 21:05:07 -0500

    Time: 1 ms (41.07%) | Memory: 58.6 MB (16.82%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -11,9 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  if (!root || root === undefined) {
-    return 0;
-  }
+    if (!root || root === undefined) return 0
 
-  return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1)
+    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1)
 };  
