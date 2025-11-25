9e89b74dadc40720df4a3994f9d8015b30e8f931
vctorye <118642061+vctorye@users.noreply.github.com>
2025-11-25 15:25:26 -0500

    Time: 34 ms (26.42%) | Memory: 54.5 MB (88.16%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,9 +5,11 @@
  */
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            let sum = nums[i] + nums[j]
-            if (sum === target) return [i,j]
+        for (let j = i + 1; j < nums.length; j++){
+            const sum = nums[i]+nums[j]
+            if (sum ===target){
+                return [i,j]
+            }
         }
     }
 };
