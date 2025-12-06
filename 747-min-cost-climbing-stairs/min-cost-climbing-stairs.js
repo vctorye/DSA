22ae6a660ed29fd212dca4d97bc61cbe873d75fe
vctorye <118642061+vctorye@users.noreply.github.com>
2025-12-06 13:20:19 -0500

    Time: 2 ms (50.49%) | Memory: 56.9 MB (15.02%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -10,8 +10,8 @@ var minCostClimbingStairs = function(cost) {
     dp[1] = cost[1];
 
     for (let i = 2; i < n; i++) {
-        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2])
     }
 
-    return Math.min(dp[n - 1], dp[n - 2])
+    return Math.min(dp[n - 1], dp[ n - 2])
 };
