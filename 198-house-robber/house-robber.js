5dae7b016fffe287e576c6b562ce180dd082dd56
vctorye <118642061+vctorye@users.noreply.github.com>
2025-12-07 20:54:27 -0500

    Time: 0 ms (100.00%) | Memory: 53.4 MB (87.41%) - LeetSync

---
+++ 198-house-robber/house-robber.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+
+  let prev2 = nums[0];
+  let prev1 = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < nums.length; i++) {
+    const current = Math.max(prev1, prev2 + nums[i]);
+    prev2 = prev1; 
+    prev1 = current;
+  }  
+
+  return prev1
+};
