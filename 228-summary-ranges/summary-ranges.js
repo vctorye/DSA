2b816ffdded247fbedab0f76718c874bc6a99994
vctorye <118642061+vctorye@users.noreply.github.com>
2025-12-16 14:03:03 -0500

    Time: 0 ms (100.00%) | Memory: 54.8 MB (9.38%) - LeetSync

--- 228-summary-ranges/summary-ranges.js
+++ 228-summary-ranges/summary-ranges.js
@@ -3,24 +3,23 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-    // base case
-    if (nums.length === 0) {
-        return []
-    }
+    if (nums.length === 0) return [];
+
     let res = [];
 
     for (let i = 0; i < nums.length; i++) {
         let start = nums[i];
 
-        while (i + 1 < nums.length && nums[i + 1] === nums[i] + 1) {
+        while (i + 1 < nums.length && nums[i + 1 ] === nums[i] + 1) {
             i++
         }
 
         if (start === nums[i]) {
-            res.push(String(start))
+            res.push(String(start));
         } else {
             res.push(`${start}->${nums[i]}`)
         }
     }
-    return res;
+
+    return res
 };
