2b84e2e5684659f1a5ba725e2538fce2910f2fd4
vctorye <118642061+vctorye@users.noreply.github.com>
2025-12-29 22:34:31 -0500

    Time: 0 ms (100.00%) | Memory: 53 MB (94.44%) - LeetSync

---
+++ 276-paint-fence/paint-fence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var numWays = function(n, k) {
+   if (n === 0) return 0; 
+   if (n === 1) return k; 
+
+   let prev2 = k;
+   let prev1 = k * k;
+
+   for (let i  = 3; i <= n; i++) {
+        const current = (prev1 + prev2) * (k - 1);
+        prev2 = prev1;
+        prev1 = current;
+   }
+   return prev1;
+};
