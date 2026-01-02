39a9c8e28cb879a8308cdc210c3838ad692e28c7
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-02 01:12:16 -0500

    Time: 15 ms (86.05%) | Memory: 61.2 MB (69.14%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -3,23 +3,22 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function(s) {
-    // use stack 
-    let array = s.split('');
     let stack = [];
+    let array = s.split('');
 
-    //iterate through array 
     for (let i = 0; i < array.length; i++) {
         if (array[i] === '(') {
-           stack.push(i); 
+            stack.push(i);
         } else if (array[i] === ')' && stack.length === 0) {
-            array[i] = '';
-        } else  if (array[i] === ")" && stack.length)  {
+            array[i] = "";
+        }   else if (array[i] === ")" && stack.length)   {
             stack.pop()
         }
     }
 
     for (let i = 0; i < stack.length; i++) {
-        array[stack[i]] = ''
+        array[stack[i]] = "";
     }
+
     return array.join('')
 };
