826353b03cbe8e9e3e579a46540cbdb3e156a443
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-12 18:33:27 -0500

    Time: 451 ms (47.83%) | Memory: 64.4 MB (83.33%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.js
+++ 286-walls-and-gates/walls-and-gates.js
@@ -3,36 +3,40 @@
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
 var wallsAndGates = function(rooms) {
-    const rows = rooms.length
-   const cols = rooms[0].length;
-   const INF = 2147483647;
-   const queue = [];
-   const directions = [[1,0], [-1,0], [0, 1], [0, -1]];
+    const rows = rooms.length;
+    const cols = rooms[0].length;
+    const INF = 2147483647;
+    const queue = [];
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-   for (let i = 0; i < rows; i++){
-        for (let j = 0; j < cols; j++) {
-            if (rooms[i][j] === 0) {
-                queue.push([i,j])
+
+    // find all gates
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (rooms[r][c] === 0) {
+                queue.push([r,c])
             }
         }
-   }
+    }
 
-   while (queue.length) {
+
+    // search going out from the gate
+    while (queue.length) {
         const [row, col] = queue.shift();
-        
-        for (let [dr,dc] of directions) {
-            const r = row + dr
-            const c = col + dc
-        
 
-        if (r < 0 || r >= rows||
-            c < 0 || c >= cols ||
-            rooms[r][c] !== INF
-        ) continue;
+        for (let [nr, nc] of directions) {
+            const r = nr + row;
+            const c = nc + col;
 
-        rooms[r][c] = rooms[row][col]  + 1;
+            if (r < 0 || r >= rows||
+                c < 0 || c >= cols||
+                rooms[r][c] !== INF 
+            ) continue;
 
-        queue.push([r, c])
+            rooms[r][c] = rooms[row][col] + 1;
+
+            queue.push([r, c])
         }
-   }
+        
+    }
 };
