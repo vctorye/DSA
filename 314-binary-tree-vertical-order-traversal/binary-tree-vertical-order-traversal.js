a9f221ea1c3e65caa3e0550632ba18e0c031878f
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-12 23:11:43 -0500

    Time: 1 ms (73.08%) | Memory: 57.5 MB (5.77%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
@@ -11,30 +11,24 @@
  * @return {number[][]}
  */
 var verticalOrder = function(root) {
-    // base case
     if (!root) return [];
 
-    // initiate variables 
     let queue = [[root, 0]];
     let map = {};
 
-    // iterate through 
-    while(queue.length) {
-        const [node, level ] = queue.shift();
-
+    while (queue.length) {
+        const [node, level] = queue.shift();
         if (!map[level]) {
             map[level] = [];
         }
 
         map[level].push(node.val);
+
         if (node.left) queue.push([node.left, level - 1])
         if (node.right) queue.push([node.right, level + 1])
-        
 
-    }  
-    // return result arayu 
-     return Object.keys(map)
-       .sort((a, b) => a - b)
-       .map((k) => map[k]);
+    }
+    return Object.keys(map).sort((a, b) => a-  b).map((k) => map[k])
 }; 
+
 
