8803cfb5e8dcadd9ed60a3d7871881df59c558a8
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-12 20:00:40 -0500

    Time: 26 ms (51.48%) | Memory: 60.6 MB (57.40%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var longestIncreasingPath = function(matrix) {
+    // variables 
+    const m = matrix.length, n = matrix[0].length;
+    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const memo = Array.from({length: m}, () => Array(n).fill(0))
+
+
+    // dfs function 
+    function dfs(r, c) {
+        if (memo[r][c] !== 0 ) return memo[r][c];
+
+        let best = 1;
+
+        for (let [dr, dc] of dirs) {
+            const nr = r + dr;
+            const nc = c + dc;
+
+            if (
+                nr >= 0 && nr < m &&
+                nc >= 0 && nc < n &&
+                matrix[nr][nc] > matrix[r][c]
+            ) {
+                best = Math.max(best, 1 + dfs(nr, nc))
+            }
+        } 
+
+        memo[r][c] = best;
+        return best
+    }
+
+
+    // iterate through matrix
+    let result = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            result = Math.max(result, dfs(i,j))
+        }
+    }
+    return result
+    
+};
