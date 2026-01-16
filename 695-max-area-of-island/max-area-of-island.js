df2b372616fc730d55085fd43df82efa08adc7b4
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-15 20:22:42 -0500

    Time: 4 ms (70.33%) | Memory: 56.5 MB (86.40%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -3,33 +3,32 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-   // set variables 
-    let rows = grid.length;
-    let cols = grid[0].length;
+    // variables 
+    const rows = grid.length;
+    const cols = grid[0].length;
     let maxArea = 0;
 
-   // set dfs function
+    // dfs funciton
     const dfs = (r, c) => {
-        // set the bounds
         if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0;
-
         grid[r][c] = 0
         let area = 1;
 
-        // recursively explore all 4 directions 
         area += dfs(r + 1, c)
         area += dfs(r - 1, c)
         area += dfs(r, c + 1)
         area += dfs(r, c - 1)
+
         return area
+
     }
-   // traverse thorugh grid
+    // iterate thorugh grid
     for (let r = 0; r < rows; r++) {
-        for (let c = 0; c <cols; c++) {
+        for (let c = 0; c < cols; c++) {
             if (grid[r][c] === 1) {
-                let area = dfs(r,c)
+                let area = dfs(r, c)
                 maxArea = Math.max(maxArea, area)
-            }
+            } 
         }
     }
     return maxArea
