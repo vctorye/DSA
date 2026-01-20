c7ccab9739ca00caf7ea1e2dc05d3a93457ca410
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-19 22:47:08 -0500

    Time: 0 ms (100.00%) | Memory: 54.2 MB (33.52%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,27 +3,20 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    // base cases 
-    if (digits.length === 0) return [];
-
-    // phone map
-    let phoneMap = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
-
+    const phoneMap = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
     let result = [];
-    // create backtrack funcion
-    function backtrack(combination, nextDigits, phoneMap, result) {
-        // if the length of the digits is 0 then push the result into resultts
+
+    const backtrack = (combination, nextDigits, phoneMap, result) => {
         if (nextDigits.length === 0) {
-            result.push(combination)
-        } else {    // else we call backtrack in a loop while slicing the first elemeent from digits
+            result.push(combination);
+        } else {
             const letters = phoneMap[nextDigits[0] - '2'];
+
             for (const letter of letters) {
-                backtrack(combination+letter, nextDigits.slice(1), phoneMap, result);
+                backtrack(combination + letter, nextDigits.slice(1), phoneMap, result)
             }
         }
     }
-
-    backtrack("", digits, phoneMap, result);
+    backtrack('', digits, phoneMap, result);
     return result
-
 };
