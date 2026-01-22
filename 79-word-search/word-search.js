abbb87fe39f414a4a8b731689c372a28ee11b666
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-22 17:57:14 -0500

    Time: 300 ms (72.55%) | Memory: 54.8 MB (56.38%) - LeetSync

--- 79-word-search/word-search.js
+++ 79-word-search/word-search.js
@@ -4,36 +4,36 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    // set variables 
     const rows = board.length, cols = board[0].length;
 
-    // dfs function
     const dfs = (r, c, i) => {
-        // chcek bounds
-        if (r < 0 || r >= rows || c < 0 ||c >= cols || board[r][c] !== word[i]) return false;
 
-        // check if string is met
+        if (r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] !== word[i]) {
+            return false
+        }
+
         if (i === word.length - 1) return true;
-        let temp =  board[r][c]
-        board[r][c] = '#'
-        // check next direction
+
+        const temp = board[r][c];
+        board[r][c] = "#"
         const found = 
-            dfs(r + 1, c, i + 1)||
-            dfs(r - 1, c, i + 1)||
-            dfs(r, c + 1, i + 1)||
-            dfs(r, c - 1, i + 1);
-        board[r][c] = temp
-        return  found
+        dfs (r + 1, c, i + 1) ||
+        dfs (r - 1, c, i + 1) || 
+        dfs (r, c + 1, i + 1) ||
+        dfs (r, c - 1, i + 1)
+
+        board[r][c] = temp;
+        return found
     }
 
-    // iterate through board 
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (board[r][c] === word[0]) {
-                if (dfs(r,c, 0)) return true; 
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (board[i][j] === word[0]) {
+                if (dfs(i, j, 0)) return true
             }
         }
     }
 
-    return false;
+    return false
 };
