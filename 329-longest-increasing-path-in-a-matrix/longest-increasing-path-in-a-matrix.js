1188dd1488730bc6165f2b5a5a940c69d91ad8d3
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-22 19:57:44 -0500

    Time: 18 ms (87.01%) | Memory: 60.8 MB (56.50%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.js
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.js
@@ -3,44 +3,36 @@
  * @return {number}
  */
 var longestIncreasingPath = function(matrix) {
-    // variables 
-    const m = matrix.length, n = matrix[0].length;
-    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    const memo = Array.from({length: m}, () => Array(n).fill(0))
+    const  m = matrix.length, n = matrix[0].length;
+    const dirs = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+    const memo = Array.from({length: m}, () => Array(n).fill(0));
+    let result = 0
 
-
-    // dfs function 
-    function dfs(r, c) {
-        if (memo[r][c] !== 0 ) return memo[r][c];
+    const dfs = (r, c) => {
+        if (memo[r][c] !== 0) return memo[r][c];
 
         let best = 1;
 
-        for (let [dr, dc] of dirs) {
+        for (let [dr, dc] of  dirs) {
             const nr = r + dr;
             const nc = c + dc;
-
             if (
                 nr >= 0 && nr < m &&
                 nc >= 0 && nc < n &&
                 matrix[nr][nc] > matrix[r][c]
-            ) {
-                best = Math.max(best, 1 + dfs(nr, nc))
+            ){
+                best = Math.max(best, 1 + dfs(nr, nc));
             }
-        } 
+        }
 
         memo[r][c] = best;
         return best
-    }
-
-
-    // iterate through matrix
-    let result = 0;
+    } 
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            result = Math.max(result, dfs(i,j))
+            result = Math.max(result, dfs(i, j)) 
         }
     }
     return result
-    
 };
