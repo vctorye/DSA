78be2cc7fcd4a28f0aa06795ff84cecff4b72290
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-26 21:44:00 -0500

    Time: 0 ms (100.00%) | Memory: 54.9 MB (7.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var climbStairs = function(n, memo = {}) {
+    if (n === 1) return 1 
+    if (n === 2) return 2
 
-
-
-    if (n === 1) return memo[1] = 1;
-    if (n === 2) return memo[2] = 2;
-
+    memo[1] = 1
+    memo[2] = 2;
+    
     if (memo[n] !== undefined) return memo[n];
 
-    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo )
     return memo[n]
 };
