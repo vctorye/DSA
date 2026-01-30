2484dc081cadeb9305f2ddba903f447086330350
vctorye <118642061+vctorye@users.noreply.github.com>
2026-01-29 19:43:45 -0500

    Time: 2 ms (69.83%) | Memory: 62.9 MB (94.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -4,7 +4,7 @@
  */
 var maxSubArray = function(nums) {
     let maxSum = nums[0];
-    let currSum = nums[0]
+    let currSum = nums[0];
 
     for (let i = 1; i < nums.length; i++) {
         currSum = Math.max(nums[i], currSum + nums[i]);
