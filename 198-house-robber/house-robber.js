2de2b8b48b60365e61ad9eceee4ca6124c84f62e
vctorye <118642061+vctorye@users.noreply.github.com>
2026-02-01 17:28:45 -0500

    Time: 0 ms (100.00%) | Memory: 53.5 MB (75.14%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var rob = function(nums) {
-  if (nums.length === 0) return 0;
-  if (nums.length === 1) return nums[0];
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
 
-  let prev2 = nums[0];
-  let prev1 = Math.max(nums[0], nums[1]);
+    prev2 = nums[0];
+    prev1 = Math.max(nums[0], nums[1]);
 
-  for (let i = 2; i < nums.length; i++) {
-    const current = Math.max(prev1, prev2 + nums[i]);
-    prev2 = prev1; 
-    prev1 = current;
-  }  
+    for (let i = 2; i < nums.length; i++) {
+        const current = Math.max(prev2 + nums[i], prev1);
+        prev2 = prev1;
+        prev1 = current;
+    }
 
-  return prev1
+    return prev1
+   
 };
