cff839baa9c7b1737ee75ee028d72fdee6b9b984
vctorye <118642061+vctorye@users.noreply.github.com>
2026-02-02 19:00:47 -0500

    Time: 10 ms (64.86%) | Memory: 54.3 MB (56.76%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumAfterPartitioning = function(arr, k) {
+    const n = arr.length;
+    const dp = Array(n + 1).fill(0);
+
+    for (let i = 0; i <= n; i++) {
+        let currMax = 0;
+
+        for (let len = 1; len <= k && i - len >= 0; len++) {
+            currMax = Math.max(currMax, arr[i - len]);
+            dp[i] = Math.max(dp[i], dp[ i - len] + currMax * len)
+        }
+    }
+
+    return dp[n]
+};
