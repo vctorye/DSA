a2e9ed94f27274cc95e7c5e2302d17d0ff51470b
vctorye <118642061+vctorye@users.noreply.github.com>
2026-02-05 22:25:24 -0500

    Time: 1 ms (98.01%) | Memory: 56.6 MB (83.76%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -3,33 +3,32 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    // variables 
-    const rows = grid.length;
-    const cols = grid[0].length;
     let maxArea = 0;
+    let rows = grid.length;
+    let cols = grid[0].length;
 
-    // dfs funciton
     const dfs = (r, c) => {
         if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0;
-        grid[r][c] = 0
+
         let area = 1;
+        grid[r][c] = 0;
 
         area += dfs(r + 1, c)
-        area += dfs(r - 1, c)
         area += dfs(r, c + 1)
+        area += dfs(r - 1, c)
         area += dfs(r, c - 1)
-
+        
         return area
-
     }
-    // iterate thorugh grid
+
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
             if (grid[r][c] === 1) {
-                let area = dfs(r, c)
-                maxArea = Math.max(maxArea, area)
-            } 
+                const area = dfs(r, c);
+                maxArea = Math.max(area, maxArea)
+            }
         }
     }
-    return maxArea
+
+    return maxArea;
 };
