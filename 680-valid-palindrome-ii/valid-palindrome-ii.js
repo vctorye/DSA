eeaa06ce46f846a50ae51b3823c75c675dfa78a0
vctorye <118642061+vctorye@users.noreply.github.com>
2026-03-09 22:12:16 -0400

    Time: 5 ms (86.39%) | Memory: 60.3 MB (92.57%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,28 +3,30 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    let left = 0
+    let left = 0;
     let right = s.length - 1;
 
     while (left < right) {
         if (s[left] !== s[right]) {
-            return isPalindrome(left + 1, right, s) || isPalindrome(left, right - 1, s)
+            return isPalindrome(left + 1, right, s) || isPalindrome(left, right - 1, s);
         } else {
-            left++
+            left++;
             right--
         }
     }
-    return true
-} 
 
-function isPalindrome(left, right, s) {
-     while (left < right) {
-        if (s[left] !== s[right]){
-            return false
-         } else {
-            left++;
-            right--;
+    return true;
+
+    
+    function isPalindrome(left, right, s) {
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
         }
+        return true
     }
-    return true
 }
