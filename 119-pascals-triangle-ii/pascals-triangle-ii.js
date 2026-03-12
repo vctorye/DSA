194f90aa27fa705ad59c6cfdd31df02dd1058216
vctorye <118642061+vctorye@users.noreply.github.com>
2026-03-12 19:56:55 -0400

    Time: 0 ms (100.00%) | Memory: 53.8 MB (58.57%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -3,12 +3,12 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    let row = [1]; // Start with first row
+    let row = [1];
 
     for (let i = 1; i <= rowIndex; i++) {
-        row.push(1); // Add 1 at the end
-        for (let j = i - 1; j > 0; j--) { // Update in reverse
-            row[j] += row[j - 1]; // Compute new values using previous row
+        row.push(1);
+        for (let j = i - 1; j > 0; j--) {
+            row[j] += row[j - 1]
         }
     }
 
