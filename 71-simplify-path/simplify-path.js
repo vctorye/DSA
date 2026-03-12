84b204e8e53d011dd5f980d2dd1b8fa81a2afca2
vctorye <118642061+vctorye@users.noreply.github.com>
2026-03-11 21:42:36 -0400

    Time: 2 ms (71.49%) | Memory: 54.5 MB (99.30%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -4,18 +4,18 @@
  */
 var simplifyPath = function(path) {
     let array = path.split('/');
-    let stack = [];
+    let stack = []; 
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === "." || array[i] === "") {
+        if (array[i] === '' || array[i] === '.') {
             continue;
         } else if (array[i] === '..') {
-            stack.pop(); 
-        } else{
+            stack.pop();
+        } else {
             stack.push(array[i])
         }
     }
 
-    return '/' + stack.join('/');
+    return '/' + stack.join('/')
 };
 
 
