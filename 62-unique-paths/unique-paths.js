2a97ed50ef6ecdc17b0dce34d8e481a76bb853fb
vctorye <118642061+vctorye@users.noreply.github.com>
2026-03-12 18:26:10 -0400

    Time: 0 ms (100.00%) | Memory: 53.4 MB (84.24%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let dp = Array(m).fill().map(() => Array(n).fill(1))
+    let dp = Array(m).fill().map(() => Array(n).fill(1));
 
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         }
-    }
-
+    } 
     return dp[m - 1][n - 1]
-
 };
 
