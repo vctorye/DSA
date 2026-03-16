cd0dae3e2e1de371fcf8c4cd819bca03a8dcc6bc
vctorye <118642061+vctorye@users.noreply.github.com>
2026-03-16 19:52:52 -0400

    Time: 311 ms (55.15%) | Memory: 55.6 MB (47.46%) - LeetSync

--- 79-word-search/word-search.js
+++ 79-word-search/word-search.js
@@ -5,32 +5,32 @@
  */
 var exist = function(board, word) {
     const rows = board.length, cols = board[0].length;
-
+    
     const dfs = (r, c, i) => {
-
         if (r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] !== word[i]) {
             return false
         }
-
-        if (i === word.length - 1) return true;
-
+        if (i === word.length - 1) return true
         const temp = board[r][c];
-        board[r][c] = "#"
-        const found = 
-        dfs (r + 1, c, i + 1) ||
-        dfs (r - 1, c, i + 1) || 
-        dfs (r, c + 1, i + 1) ||
-        dfs (r, c - 1, i + 1)
+        board[r][c] = "#";
+        
+        const output = 
+        dfs(r + 1, c, i + 1) ||
+        dfs(r, c + 1, i + 1) ||
+        dfs(r - 1, c, i + 1) ||
+        dfs(r, c - 1, i + 1)
 
-        board[r][c] = temp;
-        return found
+        board[r][c] = temp
+
+        return output
+        
+
     }
 
-
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            if (board[i][j] === word[0]) {
-                if (dfs(i, j, 0)) return true
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (board[r][c] === word[0]) {
+                if (dfs(r, c, 0)) return true
             }
         }
     }
