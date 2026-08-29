0e548511d80b20ce287b342ce752e6783143a515
vctorye <118642061+vctorye@users.noreply.github.com>
2026-08-29 14:28:28 -0400

    Time: 3 ms (70.14%) | Memory: 55.8 MB (50.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -4,22 +4,21 @@
  */
 var isValid = function(s) {
     let stack = [];
-
-    let map = {
-        ')' : '(',
-        ']' : '[',
-        '}' : '{'
+    
+    const map = {
+        '}': '{',
+        ']': '[',
+        ')': '('
     }
 
     for (let char of s) {
         if (map[char]) {
-            if (stack.length === 0 || stack.pop() !== map[char] ){
+            if (stack.length === 0 ||  stack.pop() !== map[char]) {
                 return false
             }
         } else {
             stack.push(char)
         }
     }
-
-    return stack.length === 0;
+    return stack.length === 0
 };
