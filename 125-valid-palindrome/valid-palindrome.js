ce0300fc9e3fa40152f975a9faa02be0206542af
vctorye <118642061+vctorye@users.noreply.github.com>
2026-09-21 17:40:26 -0400

    Time: 6 ms (50.79%) | Memory: 56.2 MB (85.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -5,18 +5,16 @@
 var isPalindrome = function(s) {
     const newStr = s.toLowerCase().replace(/[^a-z0-9]/g, '')
 
-    let left = 0;
+    let left =  0;
     let right = newStr.length - 1;
 
     while (left < right) {
         if (newStr[left] !== newStr[right]) {
             return false
         }
-
-        left++
+        left++;
         right--;
     }
 
     return true
-
 };
